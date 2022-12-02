57d4f0831efebed62697ddf3b52c352638dd22be
pannacosta <94145041+pannacosta@users.noreply.github.com>
2022-12-02 16:17:05 +0000

    Create 2022_day-1.py

---
+++ 2022_day-1.py
@@ -0,0 +1,21 @@
+import re
+import pandas as pd
+
+my_file = open("C:/Users/pauls/Desktop/input_2022_d1.txt", "r")
+content = my_file.read()
+puzzle_input_d1 = content.split("\n\n")
+my_file.close()
+
+for index, item in enumerate(puzzle_input_d1):
+    puzzle_input_d1[index] = item.split("\n")
+
+for index, item in enumerate(puzzle_input_d1):
+    puzzle_input_d1[index] = sum([int(value) for value in item if value != ''])
+    
+max_value = max(puzzle_input_d1)
+max_index = puzzle_input_d1.index(max_value)
+
+# pt 1 answer
+max_value
+# pt 2 answer
+sum(sorted(puzzle_input_d1, reverse=True)[:3])
